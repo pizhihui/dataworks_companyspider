43c05030c399e3e0148688856eed554e8256907e
pizh <pizh@yonyou.com>
2017-11-06 16:07:56 +0800

    公司测试代码

--- companydata/companyquery.py
+++ companydata/companyquery.py
@@ -3,18 +3,15 @@ import sys
 import hashlib
 import requests
 import json
+import os
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
 from elasticsearch import Elasticsearch
 
-def main():
-
-    outFile = open('escompany.txt', 'w')
-    needed = open('esneeded.txt', 'w')
-
-    nettyHeader = {
+nettyHeader = {
         "Accept": "*/*",
         "Accept-Encoding": "gzip, deflate",
         "Accept-Language": "zh-CN,zh;q=0.8",
@@ -23,6 +20,13 @@ def main():
         "Content-Length": "55",
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     }
+
+def main():
+
+    outFile = open('escompany.txt', 'w')
+    needed = open('esneeded.txt', 'w')
+
+
 
     es = Elasticsearch('10.10.4.148:9200')
     _query_name_contains = {
@@ -72,6 +76,29 @@ def main():
     needed.close()
     outFile.close()
 
+def hbase_query():
+    file_path_trg = os.getcwd() + "/allcompaydata_final.txt"
+    file_path_needed = os.getcwd() + "/company_needed.txt"
+
+    file_needed = open(file_path_needed, 'w')
+
+    with open(file_path_trg, 'r') as f:
+        companys = f.readlines()
+        for com in companys:
+            row_key = hashlib.new("md5", com).hexdigest()
+            data = {
+                "command": "getByRowKey",
+                "content": {
+                    "table": "raw_corp",
+                    "rowKey": row_key,
+                    "family": "info"
+                }
+            }
+            res = requests.post('http://10.10.4.158:8986', json=data, headers=nettyHeader)
+            if not json.loads(res.content)['result']:
+                file_needed.write(com)
+
+    file_needed.close()
 
 if __name__=="__main__":
-    main()
+    hbase_query()
